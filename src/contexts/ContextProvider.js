2f75e02465cd3c59980938fdccd989338a8bccb1
dpham4 <dpham4@binghamton.edu>
2022-06-07 20:24:53 +0700

    Finish Navbar, Ecommerce

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,20 +1,35 @@
-import React, {createContext, useContext, useState} from 'react'
+import React, { createContext, useContext, useState } from 'react'
 
-const StateContext = createContext(); 
+const StateContext = createContext();
 
 const initialState = {
-    chat: false, 
-    cart: false, 
+    chat: false,
+    cart: false,
     userProfile: false,
     notification: false,
-} 
+}
 
-export const ContextProvider = ( {children}) => {
+export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true)
-    return (
-        <StateContext.Provider value = {{activeMenu, setActiveMenu}}>
-            {children}
-        </StateContext.Provider>
+    const [isClicked, setIsClicked] = useState(initialState)
+    const [screenSize, setScreenSize] = useState(undefined)
+
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState, [clicked]: true })
+    }
+    return ( <
+        StateContext.Provider value = {
+            {
+                activeMenu,
+                setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick,
+                screenSize,
+                setScreenSize
+            }
+        } > { children } <
+        /StateContext.Provider>
     )
 }
 
